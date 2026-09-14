7321ed87aaf9e478aa1b87e48f5b6d466ebb96e1
Nairoby-progra <anairoby28@gmail.com>
2026-09-13 19:31:05 -0600

    Add files via upload

---
+++ clase usuario.cs
@@ -0,0 +1,14 @@
+namespace TicketsSoporte logica
+public  class Usuario
+public string str codigo {grt,} {set,};
+public string str nombre {grt,} {set,};
+public string str correo {grt,} {set,};
+public Usuario(string str codigo, string su nombre, string srt correo){
+    TMIS strcodigo - strcodigo;
+    TMIS strNombre - strNombre;
+    TMIS strcorreo - strcorreo;
+    public Abstract obtenerRol();
+
+
+    
+}
